c3fd7aa90dc2409671578905fbee222815606afb
Albert Tugushev <metalikus@gmail.com>
2013-11-26 02:04:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name='pycian',
-    version='1.0',
-    py_modules=['pycian'],
-    requires=['pyquery'],
+    version='0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    author='Albert Tugushev',
+    author_email='albert@tugushev.ru',
+    url='http://github.com/alikus/pycian',
+    description='E-mail notifications of new flats from www.cian.ru',
+    platforms='any',
+    test_suite='tests',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
